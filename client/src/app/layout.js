b35d5d84d00137691fd282e39eaf10edc8520186
stevennbui <sdbui@ucsd.edu>
2025-04-05 13:26:22 -0700

    s

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Redstone Remind',
+  title: 'Redstone Recall',
   description: 'Study with Steve',
 }
 
